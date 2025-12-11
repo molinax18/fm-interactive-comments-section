bc9bc8eeebddae90b10f8fca98948b9e1b62eb26
molinax18 <arielmolina8953@gmail.com>
2025-12-11 01:28:57 -0300

    chore: typescript alias

--- src/contexts/CommentsContext.tsx
+++ src/contexts/CommentsContext.tsx
@@ -1,10 +1,10 @@
 import { createContext, useContext, useReducer } from "react";
-import { commentReducer } from "../reducers/CommentsReducer";
-import { comments as INITIAL_STATE } from "../../data.json";
+import { commentReducer } from "@/reducers/CommentsReducer";
+import { comments as INITIAL_STATE } from "@/data.json";
 import type {
   CommentsContextProps,
   CommentsProviderProps,
-} from "../type/commentsContext.type";
+} from "@/type/commentsContext.type";
 
 const CommentsContext = createContext<CommentsContextProps | null>(null);
 
