eb2c4089fef64a63d66782f35098bafc111a79d9
molinax18 <arielmolina8953@gmail.com>
2025-12-14 16:37:43 -0300

    style: CommentCard desktop design

--- src/comment/components/CommentCard.tsx
+++ src/comment/components/CommentCard.tsx
@@ -28,21 +28,26 @@ export default function CommentCard({ data, isCurrentUser, className }: Props) {
 
   return (
     <article
-      className={`grid grid-cols-12 gap-y-3 p-3 rounded-md bg-white text-grey-500 ${className || ""}`}
+      className={`grid grid-cols-12 gap-y-3 p-3 rounded-md bg-white text-grey-500 md:gap-3 md:p-4 ${className || ""}`}
     >
-      <header className="col-span-full flex items-center gap-x-3">
+      <header className="col-span-full flex items-center gap-x-3 md:row-start-1 md:col-start-2 md:col-span-8">
         <UserProfile user={user} isCurrentUser={isCurrentUser} />
 
         <time>{createdAt}</time>
       </header>
 
-      <ScoreControl score={score} className="row-start-3 col-span-4" />
+      <ScoreControl
+        score={score}
+        className="row-start-3 col-span-4 md:col-start-1 md:col-span-1 md:row-start-1 md:row-span-3 md:flex-col md:items-center md:py-4"
+      />
 
-      <p className="row-start-2 col-span-full">{renderMessage()}</p>
+      <p className="row-start-2 col-span-full md:col-start-2">
+        {renderMessage()}
+      </p>
 
       <ActionControl
         isCurrentUser={isCurrentUser}
-        className={`${isCurrentUser ? "flex gap-x-4 col-start-8" : "col-start-10"} row-start-3 col-span-full ml-auto`}
+        className={`${isCurrentUser ? "flex gap-x-4 col-start-8" : "col-start-10"} row-start-3 col-span-full self-center ml-auto md:row-start-1`}
       />
     </article>
   );
